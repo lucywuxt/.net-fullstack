30018bc21a5cb8ebcdd256999075a347f5c6d6e0
Lucy Wu <75866185+lucywuxt@users.noreply.github.com>
2026-08-31 16:15:41 -0400

    first app

--- 8-31 - Enviornment Setup/firstapp/Program.cs
+++
@@ -1,2 +0,0 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("This is my very first project in .NET core, and its easy so far.");  
---
+++ 8-31-Enviornment Setup/firstapp/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("This is my very first project in .NET core, and its easy so far.");  
+
+string myName = "Lucy Wu";
+string myHobby = "Matcha";
+
+Console.WriteLine($"My name is {myName} and my hobby is {myHobby}.");
+
+System.Console.WriteLine("Please enter your name: ");
+string userName = System.Console.ReadLine();
+System.Console.WriteLine($"Hello, {userName}!");
