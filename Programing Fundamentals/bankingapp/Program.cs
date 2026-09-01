fe11efc2eee3212ddbf89c4bfac96f38e474fa7e
Lucy Wu <75866185+lucywuxt@users.noreply.github.com>
2026-09-01 11:02:50 -0400

    Day 2

---
+++ Programing Fundamentals/bankingapp/Program.cs
@@ -0,0 +1,53 @@
+﻿bool continueTransaction = true;
+while (continueTransaction)
+{
+
+Console.WriteLine("!~~~~~~~Welcome to Bank of America~~~~~~~!");
+Console.WriteLine("1. Create Account");
+Console.WriteLine("2. Check Balance");
+Console.WriteLine("3. Withdraw Funds");
+Console.WriteLine("4. Deposit Funds");
+Console.WriteLine("5. Transfer Funds");
+Console.WriteLine("6. View Transaction History");
+Console.WriteLine("7. Change ATM PIN");
+Console.WriteLine("8. Request Loan");
+Console.WriteLine("9. Exit");
+
+int userChoice = Convert.ToInt32(Console.ReadLine());
+switch(userChoice)
+{
+    case 1:
+        Console.WriteLine("You have selected to create a new account.");
+        break;
+    case 2:
+        Console.WriteLine("You have selected to check your balance.");
+        break;
+    case 3:
+        Console.WriteLine("You have selected to withdraw funds.");
+        break;
+    case 4:
+        Console.WriteLine("You have selected to deposit funds.");
+        break;
+    case 5:
+        Console.WriteLine("You have selected to transfer funds.");
+        break;
+    case 6:
+        Console.WriteLine("You have selected to view transaction history.");
+        break;
+    case 7:
+        Console.WriteLine("You have selected to change your ATM PIN.");
+        break;
+    case 8:
+        Console.WriteLine("You have selected to request a loan.");
+        break;
+    case 9:
+        continueTransaction = false;
+        Console.WriteLine("Thank you for using Bank of America. Goodbye!");
+        break;
+    default:
+        Console.WriteLine("Invalid choice. Please select a valid option.");
+        break;
+}
+Console.WriteLine("Press any key to continue... or 9 to exit.");
+Console.ReadKey();
+}
---
+++ Programing Fundamentals/fundamentals/Program.cs
@@ -0,0 +1,83 @@
+﻿#region data types
+
+string name = "Lucy";
+string designation = "Software Engineer";
+int age = 22;
+double salary = 20000.50;
+bool isMarried = false;
+
+#endregion
+
+#region input (also does conditional checking)
+
+Console.WriteLine("!~~~~~~~~~~~~ Welcome to CITI Bank ~~~~~~~~~~~~~!");
+Console.WriteLine("Please enter your name: ");
+string userName = Console.ReadLine();
+
+Console.WriteLine("Please enter your city: ");
+string userCity = Console.ReadLine();
+
+Console.WriteLine("Please enter your age: ");
+int userAge = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter your marital status (true/false): ");
+bool userMarried = Convert.ToBoolean(Console.ReadLine());
+
+Console.WriteLine("Thank you for providing your information. We will process it shortly.");
+
+
+#region Condition checking
+
+bool validationPassed = true;
+
+// 1. name should not be empty or null 
+// & should not contain less than 3 and more than 25 chars
+// & need to convert the name to uppercase
+if (string.IsNullOrEmpty(userName) || userName.Length < 3 || userName.Length > 25)
+{
+    validationPassed = false;
+    Console.WriteLine("Invalid name. Please enter a name with 3 to 25 characters.");
+}else{
+    userName = userName.ToUpper();
+}
+
+// 2. city should not be empty or null & can only be: "New York", "Los Angeles", "Chicago"
+if (string.IsNullOrEmpty(userCity) || (userCity != "New York" && userCity != "Los Angeles" && userCity != "Chicago"))
+{
+    validationPassed = false;
+    Console.WriteLine("Invalid city. Please enter either 'New York', 'Los Angeles', or 'Chicago'.");
+}
+
+// 3. age should be between 18 and 60 & cannot be negative or zero & cannot be a decimal number & not empty or null
+if (string.IsNullOrEmpty(userAge.ToString()) || userAge < 18 || userAge > 60)
+{
+    validationPassed = false;
+    Console.WriteLine("Invalid age. Please enter an age between 18 and 60.");
+}else if (userAge <= 0)
+{
+    validationPassed = false;
+    Console.WriteLine("Invalid age. Age cannot be negative or zero.");
+}else if (userAge % 1 != 0)
+{
+    validationPassed = false;
+    Console.WriteLine("Invalid age. Age cannot be a decimal number.");
+}
+
+if(isMarried != true && isMarried != false)
+{
+    validationPassed = false;
+    Console.WriteLine("Invalid marital status. Please enter either 'true' or 'false'.");
+}
+
+if (validationPassed)
+{
+    Console.WriteLine($"Approved! Thank you {userName} from {userCity}, age {userAge}, marital status: {userMarried}. Your information has been successfully processed.");
+}
+else
+{
+    Console.WriteLine("Some inputs are invalid. Please review the error messages above.");
+}
+
+#endregion
+
+#endregion
