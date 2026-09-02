51990d6adb141ec2add7dcbf9d6c4036854e310a
Lucy Wu <75866185+lucywuxt@users.noreply.github.com>
2026-09-02 09:53:37 -0400

    Day 3

---
+++ Day3/counter/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+Console.WriteLine("Counter Demo");
+
+int counter = int.Parse(File.ReadLines("counter.txt").LastOrDefault() ?? "0");
+
+Console.WriteLine($"You have run this program {counter + 1} times.");
+
+StreamWriter myPen = new StreamWriter("counter.txt");
+myPen.WriteLine(counter + 1);
+myPen.Close();
