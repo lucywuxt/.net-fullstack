52044dba5c0cfc3bb3b10f894806d05b5739f0c8
Lucy Wu <75866185+lucywuxt@users.noreply.github.com>
2026-09-09 09:58:47 -0400

    Day 6

---
+++ LINQ/Bank/BankApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using bankLIB;
+
+Accounts acc = new Accounts();
+acc.accBalance = 5000;
+acc.accName = "Lucy";
+acc.accNo = 101;
+
+Console.WriteLine(acc.checkBalance());
+Console.WriteLine(acc.Withdraw(800));
+Console.WriteLine(acc.Deposit(12000));
---
+++ LINQ/Bank/BankLIB/Accounts.cs
@@ -0,0 +1,26 @@
+﻿namespace bankLIB;
+
+public class Accounts
+{
+    public int accNo { get; set; }
+    public double accBalance { get; set; }
+    public string accName { get; set; } = "";
+
+
+    public double checkBalance()
+    {
+        return accBalance;
+    }
+
+    public double Withdraw(int amount)
+    {
+        accBalance = accBalance - amount;
+        return accBalance;
+    }
+
+    public double Deposit(int amount)
+    {
+        accBalance = accBalance + amount;
+        return accBalance;
+    }
+}
