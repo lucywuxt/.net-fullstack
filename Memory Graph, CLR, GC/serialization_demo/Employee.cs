d19f4008116dd9d712d46a72c80445dd29b415cd
Lucy Wu <75866185+lucywuxt@users.noreply.github.com>
2026-09-10 13:08:44 -0400

    Day 8

--- Memory Graph, CLR, GC/serialization_demo/Employee.cs
+++ Memory Graph, CLR, GC/serialization_demo/Employee.cs
@@ -1,4 +1,3 @@
-using System.Runtime.Serialization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -8,23 +7,33 @@ namespace EmpManagement
     {
         [JsonRequired]
         public int empNo { get; set; }
-        public string empName { get; set; }
+        public string empName { get; set; } = "";
         public int empAvailableLeave { get; set; }
+
+        [JsonPropertyName("MonthlyPay")]
         public double empSalary { get; set; }
+
+        [JsonPropertyName("Permenant")]
         public bool empIsActive { get; set; }
 
         [JsonIgnore]
-        public string empPassword { get; set; }
+        public string empPassword { get; set; } // password should not be saved
 
 
-        public void ApplyLeave(int leaves)
+        public int ApplyLeave(int days)
         {
-            empAvailableLeave += leaves;
+            if (days > 5)
+            {
+                throw new Exception("Sorry cannot apply for more than 5 days");
+            }
+            empAvailableLeave = empAvailableLeave - days;
+            return empAvailableLeave;
         }
 
-        public void AppriseSalary()
+        public double AppriseSalary()
         {
-            // empAvailableLeave += leaves;
+            empSalary = empSalary + 2000;
+            return empSalary;
         }
 
         public string SaveObject()
@@ -33,7 +42,7 @@ namespace EmpManagement
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
                 WriteIndented = true,
-                PropertyNamingCaseInsensitive = true
+                PropertyNameCaseInsensitive = true
             };
 
             // save this object
