5e29ca14260977da1845711a45b16a791dc9f4b1
Lucy Wu <75866185+lucywuxt@users.noreply.github.com>
2026-09-14 14:17:53 -0400

    Day 10

--- RDBMS/employeeDBTraining.sql
+++ RDBMS/employeeDBTraining.sql
@@ -16,31 +16,31 @@
 -- INSERT INTO deptInfo VALUES('IT', 'Chicago');
 -- INSERT INTO deptInfo VALUES('Training', 'New York');
 -- SELECT * FROM deptInfo
--- CREATE TABLE employeeInfo (
---     empNO          INT          IDENTITY (001, 1) PRIMARY KEY,
---     empName        VARCHAR (20) NOT NULL,
---     empDesigantion VARCHAR (20) NOT NULL,
---     empSalary      DECIMAL(7,2) NOT NULL,
---     empIsActive    BIT          NOT NULL, -- boolean type in SQL
---     empDept        VARCHAR(20)  NOT NULL,
---     empSSN         VARCHAR (11) NOT NULL UNIQUE,
---     CONSTRAINT check_empName_length CHECK (LEN(empName) >= 3),
---     CONSTRAINT check_desigantion_values CHECK (empDesigantion IN ('HR', 'Developer', 'Accountant', 'Trainer')),
---     CONSTRAINT check_empSalary CHECK (empSalary BETWEEN 8000 AND 25000), -- OR: empSalary <= 25000 and empSalary >=  8000
---     FOREIGN KEY (empDept) REFERENCES deptInfo (deptName)
--- );
+CREATE TABLE employeeInfo (
+    empNO          INT          IDENTITY (001, 1) PRIMARY KEY,
+    empName        VARCHAR (20) NOT NULL,
+    empDesignation VARCHAR (20) NOT NULL,
+    empSalary      DECIMAL(7,2) NOT NULL,
+    empIsActive    BIT          NOT NULL, -- boolean type in SQL
+    empDept        VARCHAR(20)  NOT NULL,
+    empSSN         VARCHAR (11) NOT NULL UNIQUE,
+    CONSTRAINT check_empName_length CHECK (LEN(empName) >= 3),
+    CONSTRAINT check_desigantion_values CHECK (empDesignation IN ('HR', 'Developer', 'Accountant', 'Trainer')),
+    CONSTRAINT check_empSalary CHECK (empSalary BETWEEN 8000 AND 25000), -- OR: empSalary <= 25000 and empSalary >=  8000
+    FOREIGN KEY (empDept) REFERENCES deptInfo (deptName)
+);
 
--- INSERT  INTO employeeInfo (empName, empDesigantion, empSalary, empIsActive, empDept, empSSN) VALUES 
--- ('Lucy', 'HR', 10000.00, 1, 'HR', '123-45-6789'),
--- ('Kamya', 'HR', 10000.00, 1, 'HR', '123-45-6797'),
--- ('Jay', 'HR', 10000.00, 1, 'HR', '123-45-6798'),
--- ('Kennan', 'Developer', 25000.00, 1, 'IT', '123-45-6790'),
--- ('Brandon', 'Developer', 25000.00, 1, 'IT', '123-45-6795'),
--- ('Sebastian', 'Accountant', 20000.00, 1, 'Accounts', '123-45-6793'),
--- ('Isaiah', 'Accountant', 20000.00, 1, 'Accounts', '123-45-6796'),
--- ('Angela', 'Trainer', 8000.00, 1, 'Training', '123-45-6791'),
--- ('Angela', 'Trainer', 8000.00, 1, 'Training', '123-45-6794'),
--- ('Pranav', 'Trainer', 8000.00, 1, 'Training', '123-45-6792');
+INSERT  INTO employeeInfo (empName, empDesignation, empSalary, empIsActive, empDept, empSSN) VALUES 
+('Lucy', 'HR', 10000.00, 1, 'HR', '123-45-6789'),
+('Kamya', 'HR', 10000.00, 1, 'HR', '123-45-6797'),
+('Jay', 'HR', 10000.00, 1, 'HR', '123-45-6798'),
+('Kennan', 'Developer', 25000.00, 1, 'IT', '123-45-6790'),
+('Brandon', 'Developer', 25000.00, 1, 'IT', '123-45-6795'),
+('Sebastian', 'Accountant', 20000.00, 1, 'Accounts', '123-45-6793'),
+('Isaiah', 'Accountant', 20000.00, 1, 'Accounts', '123-45-6796'),
+('Angela', 'Trainer', 8000.00, 1, 'Training', '123-45-6791'),
+('Angela', 'Trainer', 8000.00, 1, 'Training', '123-45-6794'),
+('Pranav', 'Trainer', 8000.00, 1, 'Training', '123-45-6792');
 
 SELECT *
 FROM   employeeInfo;
