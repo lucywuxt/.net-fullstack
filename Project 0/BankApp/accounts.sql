98ec2f4b5bebb200331bf352c23f2737fa47c784
Lucy Wu <75866185+lucywuxt@users.noreply.github.com>
2026-09-14 10:39:31 -0400

    project 0

--- Project 0/BankApp/accounts.sql
+++ Project 0/BankApp/accounts.sql
@@ -10,4 +10,8 @@ CREATE TABLE accounts
     username VARCHAR(30),
     password VARCHAR(30),
     accBalance DECIMAL(18, 2)
-);
+)
+insert into accounts values(111,'Peter','123', 100.00);		
+insert into accounts values(002,'Mary','123', 0.0);
+
+select * from accounts
