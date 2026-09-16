f12aa11d437d97901b784d707d1ff89e111a20ce
Lucy Wu <75866185+lucywuxt@users.noreply.github.com>
2026-09-16 18:54:18 -0400

    Coding Challenge 2

---
+++ Coding Challenge/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text.RegularExpressions;
+public class Solution
+{
+    static int longest(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return 0;
+        }
+        return input
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(w => Regex.Replace(w, @"[\p{P}\p{S}]", "").Length)
+            .DefaultIfEmpty(0)
+            .Max();
+    }
+
+    static string RemoveAllOccurrences(string paragraph, string substring)
+    {
+        if (string.IsNullOrEmpty(substring))
+            return paragraph;
+
+        return paragraph.Replace(substring, "");
+    }
+    static void Main()
+    {
+        Console.WriteLine("longest word input:");
+        string input = Console.ReadLine();
+        Console.WriteLine(longest(input));
+
+        string s1 = Console.ReadLine();
+        string s2 = Console.ReadLine();
+        Console.WriteLine(RemoveAllOccurrences(s1, s2));
+    }
+}
---
+++ Coding Challenge/Quiz_Template.cs
@@ -0,0 +1,17 @@
+// using System;
+// public class Solution
+// {
+//     static string functionName(int x, string y)
+//     {
+//         // implement the funtion here
+//         return x + y;
+//     }
+//     static void Main()
+//     {
+//         int integers = int.Parse(Console.ReadLine());
+//         // OR
+//         string strings = Console.ReadLine();
+
+//         Console.WriteLine(functionName(integers, strings));
+//     }
+// }
