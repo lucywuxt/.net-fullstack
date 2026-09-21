06e1816689d45282dc062d653ea737f5c44b37d2
Lucy Wu <75866185+lucywuxt@users.noreply.github.com>
2026-09-21 14:48:07 -0400

    Day 12 & 13

--- TSQL/triggers.sql
+++ TSQL/triggers.sql
@@ -1,48 +1,16 @@
--- create table stockInfo(
---     stockID int primary key,
---     stockName varchar(20),
---     stockPrice int,
---     availableQty int
--- )
--- insert into stockInfo values(1, 'Pepsi', 5, 100)
--- insert into stockInfo values(2, 'Coke', 5, 100)
--- insert into stockInfo values(3, 'Chips', 3, 100)
--- insert into stockInfo values(4, 'Yeti', 10, 100)
--- insert into stockInfo values(5, 'Apple', 230, 100)
+-- ======== BEFORE Trigger ========
 
--- SELECT * from stockInfo
-
-USE employeeDBTraining;
-
--- CREATE TABLE billing(
---     saleID int PRIMARY KEY,
---     stockID int FOREIGN KEY(stockID) REFERENCES stockInfo,
---     saleQty int,
---     amount int
--- )
-
--- when the user insert into billing values(101, 5, 12, variable)
-
--- CREATE TRIGGER trig_sales
--- ON billing
--- AFTER INSERT AS
--- BEGIN
---     DECLARE @prodcutPrice int;
---     SET @prodcutPrice = (SELECT stockPrice FROM stockInfo WHERE stockID = inserted.stockID)
-
--- END
-
-
-CREATE TRIGGER emp_info_BlockDML
+ALTER TRIGGER emp_info_BlockDML
 ON dbo.emp_info
 AFTER INSERT, UPDATE, DELETE
 AS
 BEGIN
-    SET NOCOUNT ON; -- stop program from counting data selected (improve performance for large #)
+    -- improve performance by stoping the database from sending "rows affected" messages back to the client after each statement
+    SET NOCOUNT ON;
 
     -- Block DML on Saturday and Sunday
     IF DATEDIFF(DAY, '19000101', CAST(GETDATE() AS date)) % 7 IN (5, 6)
-                   -- mins in a day                           5th (Sat) & 6th (Sun) day in a week
+                   -- 1900/01/01                           0 = Mon ... 5 = Sat, 6 = Sun
     BEGIN
         RAISERROR(
             'DML operations are not allowed on Saturday or Sunday.',
@@ -52,9 +20,12 @@ BEGIN
         RETURN;
     END;
 
+    DECLARE @CurrentTime time =
+    CAST(SYSDATETIMEOFFSET() AT TIME ZONE 'Eastern Standard Time' AS time);
+
     -- Block DML before 9:30 AM or after 6:00 PM
-    IF CAST(GETDATE() AS time) < '09:30:00'
-       OR CAST(GETDATE() AS time) >= '18:00:00'
+     IF @CurrentTime < '09:30:00'
+         OR @CurrentTime > '18:00:00'
     BEGIN
         RAISERROR(
             'DML operations are allowed only between 9:30 AM and 6:00 PM.',
@@ -64,4 +35,82 @@ BEGIN
         RETURN;
     END;
 END;
-GO
+
+
+---------------------------------------------------------------
+-- ======== AFTER Trigger ========
+
+Stock Info Table
+create table stockInfo(
+    stockID int primary key,
+    stockName varchar(20),
+    stockPrice int,
+    availableQty int
+)
+insert into stockInfo values(1, 'Pepsi', 5, 100)
+insert into stockInfo values(2, 'Coke', 5, 100)
+insert into stockInfo values(3, 'Chips', 3, 100)
+insert into stockInfo values(4, 'Yeti', 10, 100)
+insert into stockInfo values(5, 'Apple', 230, 100)
+
+SELECT * from stockInfo
+
+Sales Info Table
+CREATE TABLE salesInfo
+(
+    saleID INT PRIMARY KEY,
+    stockID INT FOREIGN KEY(stockID) REFERENCES stockInfo,
+    saleQty INT NOT NULL,
+    saleTime DATETIME NOT NULL DEFAULT GETDATE()
+)
+
+Update available qty automatically after a sale
+CREATE TRIGGER trigger_salesInfo_UpdateStock
+ON salesInfo
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE S
+    SET S.availableQty = S.availableQty - I.TotalSaleQty
+    FROM StockInfo S
+    INNER JOIN
+    (
+        SELECT stockID, SUM(saleQty) AS TotalSaleQty
+        FROM inserted
+        GROUP BY stockID
+    ) I
+        ON S.stockID = I.stockID;
+END;
+
+-- sales 12 apples
+insert into salesInfo values(901,5,12,GETDATE())
+select * from stockInfo
+
+
+---------------------------------------------------------------
+-- ======== INSTEAD OF Trigger ========
+
+create view emp_dept_view
+as
+select empNo, empName, empDesignation, empSalary, empDeptNo, empManager, dept_info.DeptNo, dept_info.DeptName, dept_info.DeptLocation
+from emp_info
+left join dept_info
+on emp_info.empDeptNo = dept_info.DeptNo
+
+select * from emp_dept_view
+
+-- can NOT insert values to a join view
+insert into emp_dept_view (empName, empDesignation, empSalary, empDeptNo, empManager) values('Hecter', 'Accountant', 15000, 30, 15)
+
+create trigger instead_of_emp_dept_view
+on emp_dept_view
+instead of insert 
+as
+begin
+insert into emp_info (empName, empDesignation, empSalary, empDeptNo, empManager)
+select empName, empDesignation, empSalary, empDeptNo, empManager from inserted
+end
+
+select * from emp_info
